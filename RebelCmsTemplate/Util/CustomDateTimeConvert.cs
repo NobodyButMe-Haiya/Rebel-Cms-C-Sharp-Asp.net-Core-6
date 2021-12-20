a48004ac391b5688815fb5b3c41c1e4bd97b7206
user <38270090+NobodyButMe-Haiya@users.noreply.github.com>
2021-12-20 19:48:35 +0800

    youtuber
    update

---
+++ RebelCmsTemplate/Util/CustomDateTimeConvert.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace RebelCmsTemplate.Util
+{
+    public class CustomDateTimeConvert
+    {
+        public static DateOnly ConvertToDate(DateTime dateString)
+        {
+            return new DateOnly(dateString.Year, dateString.Month, dateString.Day);
+        }
+        public static TimeOnly ConvertToTime(TimeSpan timeString)
+        {
+            return new TimeOnly(timeString.Hours, timeString.Minutes);
+        }
+    }
+}
+
